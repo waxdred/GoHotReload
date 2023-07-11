33711f6bdddcc0dd74633d2ec3e24f4dbee23da6
waxcoin <waxcoin@hotmail.com>
2023-07-11 23:38:54 +0200

    update parsing add struct [] for multi program

--- main.go
+++ main.go
@@ -2,63 +2,52 @@ package main
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
 	"path/filepath"
-	"strings"
-	"syscall"
 	"time"
 )
 
-type Config struct {
-	Cmd       string
-	Interval  string
-	Extension string
-	Path      string
-}
-
 func PrintBox(app *App) {
-	top := "┌───────────────────────────────────────────────────┐\n"
-	bottom := "└───────────────────────────────────────────────────┘\n"
-	cmd := fmt.Sprintf("│  cmd .......... %s", app.Cmd)
-	extension := fmt.Sprintf("│  extension .... %s", app.Extension)
-	path := fmt.Sprintf("│  path ......... %s", app.Path)
-	pid := fmt.Sprint("│  pid ........... ", app.Pid)
-	fmt.Printf(top)
-	fmt.Printf("│                    GoHotRelaod                    │\n")
-	fmt.Printf("│                                                   │\n")
-	fmt.Printf(cmd)
-	fmt.Printf(strings.Repeat(" ", (54 - len(cmd))))
-	fmt.Printf("│\n")
-	fmt.Printf(path)
-	fmt.Printf(strings.Repeat(" ", (54 - len(path))))
-	fmt.Printf("│\n")
-	fmt.Printf(pid)
-	fmt.Printf(strings.Repeat(" ", (54 - len(extension))))
-	fmt.Printf("│\n")
-	fmt.Printf(extension)
-	fmt.Printf(strings.Repeat(" ", (54 - len(extension))))
-	fmt.Printf("│\n")
+	// top := "┌───────────────────────────────────────────────────┐\n"
+	// bottom := "└───────────────────────────────────────────────────┘\n"
+	// cmd := fmt.Sprintf("│  cmd .......... %s", app.Cmd)
+	// extension := fmt.Sprintf("│  extension .... %s", app.Extension)
+	// path := fmt.Sprintf("│  path ......... %s", app.Path)
+	// pid := fmt.Sprint("│  pid ........... ", app.Pid)
+	// fmt.Printf(top)
+	// fmt.Printf("│                    GoHotRelaod                    │\n")
+	// fmt.Printf("│                                                   │\n")
+	// fmt.Printf(cmd)
+	// fmt.Printf(strings.Repeat(" ", (54 - len(cmd))))
+	// fmt.Printf("│\n")
+	// fmt.Printf(path)
+	// fmt.Printf(strings.Repeat(" ", (54 - len(path))))
+	// fmt.Printf("│\n")
+	// fmt.Printf(pid)
+	// fmt.Printf(strings.Repeat(" ", (54 - len(extension))))
+	// fmt.Printf("│\n")
+	// fmt.Printf(extension)
+	// fmt.Printf(strings.Repeat(" ", (54 - len(extension))))
+	// fmt.Printf("│\n")
 
-	fmt.Println(bottom)
+	// fmt.Println(bottom)
 }
 
-func Handler(app *App) bool {
+func Handler(prog *Program) bool {
 	files := make(map[string]time.Time)
 	update := false
 
-	err := filepath.Walk(app.Path, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(prog.Path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if !info.IsDir() && filepath.Ext(path) == app.Extension {
+		// fmt.Println("foo", prog.Extension, info.IsDir(), filepath.Ext(path), prog.Path)
+		if !info.IsDir() && filepath.Ext(path) == prog.Extension {
 			files[path] = info.ModTime()
 			time1, ok1 := files[path]
-			time2, ok2 := app.Files[path]
+			time2, ok2 := prog.Files[path]
 			if ok1 && ok2 && !time1.Equal(time2) {
 				fmt.Println(path, "Is Update")
 				update = true
@@ -66,7 +55,7 @@ func Handler(app *App) bool {
 		}
 		return nil
 	})
-	app.Files = files
+	prog.Files = files
 	if err != nil {
 		fmt.Println("Error:", err)
 		return false
@@ -74,8 +63,8 @@ func Handler(app *App) bool {
 	return update
 }
 
-func KillPid(app *App) error {
-	err := app.Process.Signal(os.Kill)
+func KillPid(prog *Program) error {
+	err := prog.Process.Signal(os.Kill)
 	if err != nil {
 		fmt.Println("Kill process error:", err)
 		return err
@@ -84,76 +73,27 @@ func KillPid(app *App) error {
 	return nil
 }
 
-func process(app *App) {
-	sigs := make(chan os.Signal, 1)
-	ticker := time.NewTicker(app.Interval)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
-
-	go func() {
-		for {
-			select {
-			case <-sigs:
-				fmt.Println("Thank see you next time...")
-				return
-			case <-ticker.C:
-				fmt.Println("Running")
-				update := Handler(app)
-				if update {
-					KillPid(app)
-					return
-				}
-				fmt.Println("update:", update)
-			}
-		}
-	}()
-	<-sigs
-}
-
-//go:embed config.json
-var configFile []byte
-
-func ParseConfig(app *App) ([]Config, error) {
-	var (
-		config []Config
-		err    error
-	)
-	if err = json.Unmarshal(configFile, &config); err != nil {
-		log.Println(err.Error())
-		return nil, err
-	}
-
-	return config, nil
-}
-
 func main() {
-	var (
-		app    = New()
-		err    error
-		config []Config
-	)
-	config, err = ParseConfig(app)
-	log.Println(config)
-	if err != nil {
-		fmt.Println("Please check your config")
-		os.Exit(-1)
-	}
+	app := New()
+	if app.Errror() != nil {
+		fmt.Println(app.Errror())
 
-	PrintBox(app)
-	ok := strings.HasPrefix(app.Extension, ".")
-	if !ok {
-		fmt.Println("Please use correct extenstion:")
 		os.Exit(-1)
 	}
-	if app.Cmd == "" {
-		fmt.Println("Please add command with flag -cmd")
+	app.CheckingParse()
+	if app.Errror() != nil {
+		app.Mu.Lock()
+		fmt.Println(app.Errror())
+		app.Mu.Unlock()
 		os.Exit(-1)
 	}
-	fmt.Println("info", app.Cmd)
-	err = app.Start().Errror()
+	fmt.Println(app.Program)
+	// PrintBox(app)
+	err := app.Start().Errror()
 	if err != nil {
+		app.Mu.Lock()
 		fmt.Println(err)
+		app.Mu.Unlock()
 		os.Exit(-1)
 	}
-	process(app)
 }
--- models.go
+++ models.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/signal"
 	"path"
+	"strings"
+	"sync"
+	"syscall"
+
+	// "path"
 	"time"
 
 	"github.com/mitchellh/go-ps"
 )
 
 type App struct {
+	Program []Program
+	Mu      sync.Mutex
+	error   error
+}
+
+type Program struct {
 	Pid        int
-	Executable string
 	Process    *os.Process
-	Path       string
-	Extension  string
-	Cmd        string
-	Interval   time.Duration
 	Files      map[string]time.Time
-	error      error
+	Path       string `json:"path"`
+	Executable string
+	Extension  string `json:"extension"`
+	Cmd        string `json:"cmd"`
+	Interval   int    `json:"interval"`
+	pid        chan bool
 }
 
 func New() *App {
-	return &App{}
+	app := &App{}
+	app.Mu.Lock()
+	defer app.Mu.Unlock()
+	fmt.Println("Parsing config ...")
+	data, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		app.error = err
+		return app
+	}
+	err = json.Unmarshal(data, &app.Program)
+	if err != nil {
+		app.error = err
+		return app
+	}
+	return app
 }
 
 func (app *App) Errror() error {
@@ -30,25 +59,98 @@ func (app *App) Errror() error {
 }
 
 func (app *App) checkPath() *App {
-	_, err := os.Stat(app.Path)
-	if err != nil {
-		app.error = err
-	}
-	if os.IsNotExist(err) {
-		fmt.Println("Path not found")
-		app.error = err
+	for _, prog := range app.Program {
+		_, err := os.Stat(prog.Path)
+		if err != nil {
+			app.error = err
+			break
+		}
+		if os.IsNotExist(err) {
+			app.error = err
+			break
+		}
 	}
 	return app
 }
 
-func (app *App) handlerProcess() *App {
-	app.Process, app.error = os.FindProcess(app.Pid)
-	fmt.Println(app.Process)
-	if app.error != nil {
-		fmt.Println("Can't found Process")
-		return app
+func (app *App) CheckingParse() *App {
+	for i := range app.Program {
+		prog := &app.Program[i]
+		if prog.Cmd == "" {
+			app.error = errors.New("Please add command")
+			break
+		} else if prog.Interval == 0 {
+			prog.Interval = 4
+		} else if prog.Extension == "" {
+			prog.Extension = ".go"
+		} else if prog.Path == "" {
+			prog.Path = "./"
+		}
+		ok := strings.HasPrefix(prog.Extension, ".")
+		if !ok {
+			app.error = errors.New("Please use correct extenstion:")
+			break
+		}
+		ok = strings.HasPrefix(prog.Path, "~")
+		if ok {
+			homeDire, err := os.UserHomeDir()
+			if err != nil {
+				app.error = err
+				break
+			}
+			prog.Path = strings.TrimPrefix(prog.Path, "~")
+			prog.Path = fmt.Sprint(homeDire, prog.Path)
+		}
 	}
 	return app
+}
+
+func (app *App) handlerProcess(prog *Program) *App {
+	prog.Process, app.error = os.FindProcess(prog.Pid)
+	if app.error != nil {
+		fmt.Println("Can't found Process")
+		os.Exit(-1)
+	}
+	return app
+}
+
+func (app *App) getExectutable() *App {
+	for i := range app.Program {
+		prog := &app.Program[i]
+		if prog.Path == "." || prog.Path == "./" {
+			pwd, _ := os.Getwd()
+			prog.Executable = path.Base(pwd)
+		} else {
+			prog.Executable = path.Base(prog.Path)
+		}
+	}
+	return app
+}
+
+func (app *App) process(prog *Program) {
+	sigs := make(chan os.Signal, 1)
+	ticker := time.NewTicker(time.Duration(prog.Interval) * time.Second)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	go func() {
+		for {
+			select {
+			case <-sigs:
+				fmt.Println("Thank see you next time...")
+				return
+			case <-ticker.C:
+				fmt.Println("Running")
+				update := Handler(prog)
+				if update {
+					KillPid(prog)
+					return
+				}
+				fmt.Println("update:", update)
+			}
+		}
+	}()
+	<-sigs
 }
 
 func (app *App) Start() *App {
@@ -56,34 +158,53 @@ func (app *App) Start() *App {
 	if err != nil {
 		return app
 	}
-	app.Executable = path.Base(app.Path)
-	pid := make(chan bool)
-	ticker := time.NewTicker(app.Interval)
+	app.getExectutable()
 
-	go func() {
-		for {
-			select {
-			case <-pid:
-				return
-			case <-ticker.C:
-				fmt.Println("Checking for PID...")
-				processes, err := ps.Processes()
-				if err != nil {
-					fmt.Println("Error:", err)
-					os.Exit(1)
-				}
-				for _, process := range processes {
-					if process.Executable() == app.Executable {
-						fmt.Printf("PID found: %d\n", process.Pid())
-						app.Pid = process.Pid()
-						pid <- true
-						return
+	var wg sync.WaitGroup
+	for i := range app.Program {
+		prog := &app.Program[i]
+		prog.pid = make(chan bool)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ticker := time.NewTicker(time.Duration(prog.Interval) * time.Second)
+			routine := false
+			for {
+				select {
+				case <-time.After(30 * time.Second):
+					fmt.Println("Timeout reached for", prog.Executable)
+					return
+				case <-prog.pid:
+					fmt.Println("Start process")
+					app.handlerProcess(prog)
+					app.process(prog)
+					fmt.Println("routine done need run again")
+					routine = false
+				case <-ticker.C:
+					if !routine {
+						fmt.Println("Checking", prog.Executable, "for PID...")
+						processes, err := ps.Processes()
+						if err != nil {
+							fmt.Println("Error:", err)
+							os.Exit(1)
+						}
+						for _, process := range processes {
+							if process.Executable() == prog.Executable {
+								fmt.Printf("%s: PID found: %d\n", prog.Executable, process.Pid())
+								prog.Pid = process.Pid()
+								routine = true
+								break
+							}
+						}
+						if routine {
+							prog.pid <- true
+						}
 					}
 				}
 			}
-		}
-	}()
-	<-pid
-	app.handlerProcess()
+		}()
+	}
+
+	wg.Wait()
 	return app
 }
